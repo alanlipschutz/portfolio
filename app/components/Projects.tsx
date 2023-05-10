c5f0d6ca51465c62227193d96b63bb3803a614e3
alanlipschutz <alanelipschutz@gmail.com>
2023-05-10 16:45:23 +0300

    feat: added images of projects.

---
+++ app/components/Projects.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Projects() {
+  return (
+    <>
+      <h3 className='text-textGreen text-3xl text-end font-mono font-bold'>
+        Projects
+      </h3>
+      <div className='w-full flex flex-col items-center justify-center gap-28 mt-10'>
+        <div></div>
+        <div></div>
+      </div>
+    </>
+  );
+}
---
+++ public/assets/index.ts
@@ -0,0 +1,15 @@
+import budget_trucker from './project-images/budget-tracker.jpeg';
+import ecommerce from './project-images/ecommerce.jpeg';
+import pet_adoption from './project-images/pet_adoption.jpeg';
+import todoapp from './project-images/todoapp.jpeg';
+import weatherApp from './project-images/weatherapp.jpeg';
+import wiseBet from './project-images/wisebet.jpeg';
+
+export {
+  budget_trucker,
+  ecommerce,
+  pet_adoption,
+  todoapp,
+  weatherApp,
+  wiseBet,
+};
