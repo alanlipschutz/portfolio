7a26ab31773ed8b117ea3d650302558ca652c31c
alanlipschutz <alanelipschutz@gmail.com>
2023-05-16 16:02:16 +0300

    feat: added animation for sections

--- app/page.tsx
+++ app/page.tsx
@@ -41,6 +41,7 @@ export default function Home() {
       }
       setActiveSection(activeSection);
       setScrolledSections(scrolledSections);
+      console.log(scrolledSections);
     };
 
     if (mainPage) {
@@ -50,53 +51,65 @@ export default function Home() {
       };
     }
   }, []);
+  const loadFeatures = () =>
+    import('./components/features').then((res) => res.default);
 
   return (
-    <main
-      id='main'
-      className='w-full h-screen bg-bodyColor text-textLight  overflow-x-hidden'
-    >
-      <Navbar activeSection={activeSection} />
-      <section
-        id='about'
-        className={`p-4 transform translate-y-10  transition-transform duration-1000 max-w-containerSmall mx-auto py-10 mdl:py-24 flex flex-col gap-4 lgl:gap-8 mdl:px-10 xl:px-4 ${
-          scrolledSections.includes('about')
-            ? 'opacity-100 translate-y-0'
-            : 'opacity-0'
-        }`}
+    <LazyMotion strict features={loadFeatures}>
+      <main
+        id='main'
+        className='w-full h-screen bg-bodyColor text-textLight  overflow-x-hidden'
       >
-        <About />
-      </section>
-      <section
-        id='skills'
-        className={`p-4 transform opacity-0 -translate-y-10  transition-transform duration-1000 max-w-containerSmall mx-auto py-10 mdl:py-24 flex flex-col gap-4 lgl:gap-8 mdl:px-10 xl:px-4 ${
-          scrolledSections.includes('skills')
-            ? 'opacity-100 translate-y-0'
-            : 'opacity-0'
-        }`}
-      >
-        <Skills />
-      </section>
-      <section
-        id='projects'
-        className={`p-4 transform opacity-0 -translate-y-10  transition-transform duration-1000 max-w-containerSmall mx-auto py-10 mdl:py-24 flex flex-col gap-4 lgl:gap-8 mdl:px-10 xl:px-4 ${
-          scrolledSections.includes('projects')
-            ? 'opacity-100 translate-y-0'
-            : 'opacity-0'
-        }`}
-      >
-        <Projects />
-      </section>
-      <section
-        id='contact'
-        className={`p-4 transform opacity-0 -translate-y-10  transition-transform duration-1000 max-w-containerSmall mx-auto py-10 mdl:py-24 flex flex-col gap-4 lgl:gap-8 mdl:px-10 xl:px-4  ${
-          scrolledSections.includes('contact')
-            ? 'opacity-100 translate-y-0'
-            : 'opacity-0'
-        }`}
-      >
-        <Contact />
-      </section>
-    </main>
+        <Navbar activeSection={activeSection} />
+        <m.section
+          id='about'
+          initial={{ opacity: 0, translateY: 20 }}
+          animate={{
+            opacity: scrolledSections.includes('about') ? 1 : 0,
+            translateY: scrolledSections.includes('about') ? -20 : -10,
+          }}
+          transition={{ duration: 0.2, delay: 0.2 }}
+          className={`p-4 transform  transition-transform duration-1000 max-w-containerSmall mx-auto py-10 mdl:py-24 flex flex-col gap-4 lgl:gap-8 mdl:px-10 xl:px-4 `}
+        >
+          <About />
+        </m.section>
+        <m.section
+          id='skills'
+          className={`p-4 transform opacity-0 transition-transform duration-1000 max-w-containerSmall mx-auto py-10 mdl:py-24 flex flex-col gap-4 lgl:gap-8 mdl:px-10 xl:px-4 `}
+          initial={{ opacity: 0, translateY: 30 }}
+          animate={{
+            opacity: scrolledSections.includes('skills') ? 1 : 0,
+            translateY: scrolledSections.includes('skills') ? -20 : -10,
+          }}
+          transition={{ duration: 0.2 }}
+        >
+          <Skills />
+        </m.section>
+        <m.section
+          id='projects'
+          className={`p-4 transform opacity-0 transition-transform duration-1000 max-w-containerSmall mx-auto py-10 mdl:py-24 flex flex-col gap-4 lgl:gap-8 mdl:px-10 xl:px-4 `}
+          initial={{ opacity: 0, translateY: 30 }}
+          animate={{
+            opacity: scrolledSections.includes('projects') ? 1 : 0,
+            translateY: scrolledSections.includes('projects') ? -20 : -10,
+          }}
+          transition={{ duration: 0.2 }}
+        >
+          <Projects />
+        </m.section>
+        <m.section
+          id='contact'
+          className={`p-4 transform opacity-0 transition-transform duration-1000 max-w-containerSmall mx-auto py-10 mdl:py-24 flex flex-col gap-4 lgl:gap-8 mdl:px-10 xl:px-4 `}
+          initial={{ opacity: 0, translateY: 30 }}
+          animate={{
+            opacity: scrolledSections.includes('projects') ? 1 : 0,
+            translateY: scrolledSections.includes('projects') ? -20 : -10,
+          }}
+          transition={{ duration: 0.2 }}
+        >
+          <Contact />
+        </m.section>
+      </main>
+    </LazyMotion>
   );
 }
