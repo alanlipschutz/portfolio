25578fe41d52afa266fe8ba476d6f903f6027ecf
alanlipschutz <alanelipschutz@gmail.com>
2023-05-16 16:51:01 +0300

    feat: added env variables

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    EMAIL: process.env.EMAIL,
+    PHONE: process.env.PHONE,
+  },
+};
 
 module.exports = nextConfig;
