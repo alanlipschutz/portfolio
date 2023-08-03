45e71ad8b5aef9590a27a30bd9940e51b60a1146
Alan <alan.l@marginx.ai>
2023-08-03 11:44:42 +0300

    feat: added google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
-import './globals.css';
+import Head from "next/head";
+import "./globals.css";
+import Script from "next/script";
 
 export const metadata = {
-  title: 'Alan Lipschutz',
+  title: "Alan Lipschutz",
   description: "Alan Lipschutz's portfolio",
 };
 
@@ -11,7 +13,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
+    <html lang="en">
+      <Head>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-CBWLNVNK83"
+        ></Script>
+        <Script>
+          {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-CBWLNVNK83');`}
+        </Script>
+      </Head>
       <body suppressHydrationWarning={true}>{children}</body>
     </html>
   );
